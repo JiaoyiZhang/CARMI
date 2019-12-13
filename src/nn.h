36c63c5d5d33d97bbc3a33799bc888ce08232fea
JY <459294193@qq.com>
2019-12-13 19:57:36 +0800

    improve neural network

--- src/nn.h
+++ src/nn.h
@@ -1,158 +1,18 @@
-// #ifndef NN_H
-// #define NN_H
+#ifndef NN_H
+#define NN_H
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 #include <chrono>
 #include <random>
 using namespace std;
 
-class net
-{
-public:
-    net(vector<pair<int, int>> dataset, int maxEpochNumber, float lr, int neuronNumber)
-    {
-        m_dataset = dataset;
-        // m_batchSize = batchSize;
-        m_learningRate = lr;
-        m_maxEpochNumber = maxEpochNumber;
-        m_neuronNumber = neuronNumber;
-        for (int i = 0; i < dataset.size(); i++)
-        {
-            index.push_back(i);
-        }
-    }
-
-    void train();
-
-    float predict(int key); // return the key's index
-
-    vector<float> Relu(vector<float> input);
-
-private:
-    vector<pair<int, int>> m_dataset;
-    vector<int> index;
-    vector<float> W1;
-    vector<float> W2;
-    vector<float> b1;
-    float b2;
-    // int m_batchSize;
-    int m_maxEpochNumber;
-    float m_learningRate;
-    int m_neuronNumber;
-};
-
-// train the network
-void net::train()
-{
-    int datasetSize = m_dataset.size();
-    // initialize the parameters
-    for (int i = 0; i < m_neuronNumber; i++)
-    {
-        W1.push_back(0);
-        W2.push_back(0);
-        b1.push_back(0);
-    }
-    b2 = 0;
-
-    cout << "Start training the first layer" << endl;
-
-    for (int epoch = 0; epoch < m_maxEpochNumber; epoch++)
-    {
-        cout << "epoch: " << epoch;
-
-        unsigned seed = chrono::system_clock::now().time_since_epoch().count();
-        shuffle(m_dataset.begin(), m_dataset.end(), default_random_engine(seed));
-        shuffle(index.begin(), index.end(), default_random_engine(seed));
-
-        float totalLoss = 0;
-        float loss = 0;
-
-        for (int i = 0; i < datasetSize; i++)
-        {
-            // forward
-            int y = index[i];                    // the actual index
-            float x = float(m_dataset[i].first); // key
-            vector<float> tempFLR = add(multiply(x, W1), b1);
-            vector<float> firstLayerResult = Relu(tempFLR); // the result of first layer
-            float tempY = mul(firstLayerResult, W2) + b2;
-            float p = max(float(0), tempY); // the result of the nn
-
-            // calculate the loss
-            loss = 0.5 * (p - float(y)) * (p - float(y));
-            totalLoss += loss;
-
-            // backward propogation
-            // update W2 and b2, if tempY <= 0, W2 = W2, b2 = b2
-            if (tempY > 0)
-            {
-                W2 = add(W2, (multiply(-m_learningRate, firstLayerResult)));
-                b2 = b2 - 1;
-            }
-
-            // updata W1 and b1
-            for (int j = 0; j < tempFLR.size(); j++)
-            {
-                if (tempY > 0 && tempFLR[i])
-                {
-                    W1[i] = W1[i] - m_learningRate * x;
-                    b1[i] = b1[i] - 1;
-                }
-            }
-
-            // print the bp result
-            cout << "now W2 is:" << endl;
-            for (int m = 0; m < W2.size(); m++)
-            {
-                cout << W2[m] << " ";
-                if (m + 1 % 10 == 0)
-                    cout << endl;
-            }
-            cout << endl;
-            cout << "now W1 is:" << endl;
-            for (int m = 0; m < W1.size(); m++)
-            {
-                cout << W1[m] << " ";
-                if (m + 1 % 10 == 0)
-                    cout << endl;
-            }
-            cout << endl;
-            cout << "now b2 is:" << b2 << endl;
-            cout << "now b1 is:" << endl;
-            for (int m = 0; m < b1.size(); m++)
-            {
-                cout << b1[m] << " ";
-                if (m + 1 % 10 == 0)
-                    cout << endl;
-            }
-            cout << endl;
-        }
-        cout << " " << totalLoss << endl;
-    }
-}
-
-float net::predict(int key)
-{
-    float x = float(key); // key
-    vector<float> firstLayerResult = Relu(add(multiply(x, W1), b1)); // the result of first layer
-    float p = max(float(0), mul(firstLayerResult, W2) + b2); // the result of the nn
-    return p;
-}
-
-vector<float> net::Relu(vector<float> input)
-{
-    for (int i = 0; i < input.size(); i++)
-    {
-        input[i] = max(float(0), input[i]);
-    }
-    return input;
-}
-
 // (1, k) * (k, 1)
-float mul(vector<float> a, vector<float> b)
+double mul(vector<double> a, vector<double> b)
 {
-    float res = 0;
+    double res = 0;
     for (int i = 0; i < a.size(); i++)
     {
         res += a[i] * b[i];
@@ -161,9 +21,9 @@ float mul(vector<float> a, vector<float> b)
 }
 
 // (1, 1) * (1, k)
-vector<float> multiply(float num, vector<float> a)
+vector<double> multiply(double num, vector<double> a)
 {
-    vector<float> res;
+    vector<double> res;
     for (int i = 0; i < a.size(); i++)
     {
         res.push_back(num * a[i]);
@@ -172,7 +32,7 @@ vector<float> multiply(float num, vector<float> a)
 }
 
 // add (1, k) and b
-vector<float> add(vector<float> a, float bias)
+vector<double> add(vector<double> a, double bias)
 {
     for (int i = 0; i < a.size(); i++)
     {
@@ -182,7 +42,7 @@ vector<float> add(vector<float> a, float bias)
 }
 
 // add (1, k) and b(1, k)
-vector<float> add(vector<float> a, vector<float> bias)
+vector<double> add(vector<double> a, vector<double> bias)
 {
     for (int i = 0; i < a.size(); i++)
     {
@@ -191,4 +51,128 @@ vector<float> add(vector<float> a, vector<float> bias)
     return a;
 }
 
-// #endif
+class net
+{
+public:
+	net(vector<pair<double, double>> dataset, int maxEpochNumber, double lr, int neuronNumber)
+	{
+		m_dataset = dataset;
+		// m_batchSize = batchSize;
+		m_learningRate = lr;
+		m_maxEpochNumber = maxEpochNumber;
+		m_neuronNumber = neuronNumber;
+		datasetSize = m_dataset.size();
+		for (int i = 0; i < datasetSize; i++)
+		{
+			index.push_back(double(i) / double(datasetSize));
+		}
+	}
+
+	void train();
+
+	double predict(int key); // return the key's index
+
+	vector<double> Relu(vector<double> input);
+
+	void print();
+
+private:
+	vector<pair<double, double>> m_dataset;
+	vector<double> index;
+	vector<double> W1;
+	vector<double> W2;
+	vector<double> b1;
+	double b2;
+	int m_maxEpochNumber;
+	double m_learningRate;
+	int m_neuronNumber;
+	int datasetSize;
+};
+
+// train the network
+void net::train()
+{
+	// initialize the parameters
+	std::default_random_engine gen;
+	std::normal_distribution<double> dis(1, 3);
+	for (int i = 0; i < m_neuronNumber; i++)
+	{
+		W1.push_back(dis(gen));
+		W2.push_back(dis(gen));
+		b1.push_back(1);
+	}
+	b2 = 0;
+
+	cout << "Start training" << endl;
+
+	for (int epoch = 0; epoch < m_maxEpochNumber; epoch++)
+	{
+		cout << endl
+			 << "epoch: " << epoch << endl;
+
+		unsigned seed = chrono::system_clock::now().time_since_epoch().count();
+		shuffle(m_dataset.begin(), m_dataset.end(), default_random_engine(seed));
+		shuffle(index.begin(), index.end(), default_random_engine(seed));
+
+		double totalLoss = 0.0;
+		double loss = 0.0;
+
+		for (int i = 0; i < datasetSize; i++)
+		{
+			// forward
+			double y = index[i];				   // the actual index
+			double x = double(m_dataset[i].first); // key
+			vector<double> tempFLR = add(multiply(x, W1), b1);
+			vector<double> firstLayerResult = Relu(tempFLR); // the result of first layer
+			double p = mul(firstLayerResult, W2) + b2;		 // the result of the nn
+
+			if (p < 0)
+				p = 0;
+			if (p > 1)
+				p = 1;
+
+			// calculate the loss
+			loss = 0.5 * (p - double(y)) * (p - double(y));
+			totalLoss += loss;
+
+			// backward propogation
+			// updata W1 and b1
+			for (int j = 0; j < W1.size(); j++)
+			{
+				if (tempFLR[j] > 0)
+				{
+					W1[j] = W1[j] - m_learningRate * x * W2[j] * (p - y);
+					b1[j] = b1[j] - m_learningRate * W2[j] * (p - y);
+				}
+			}
+			// update W2 and b2
+			W2 = add(W2, (multiply(-m_learningRate, multiply(p - y, firstLayerResult)))); // W2 = W2 - lr * firstLayerResult * (p - y)
+			b2 = b2 - m_learningRate * (p - y);
+		}
+		cout << " loss is: " << setiosflags(ios::fixed) << setprecision(4) << totalLoss << endl;
+	}
+}
+
+double net::predict(int key)
+{
+	double x = double(key);											  // key
+	vector<double> firstLayerResult = Relu(add(multiply(x, W1), b1)); // the result of first layer
+	double p = mul(firstLayerResult, W2) + b2;						  // the result of the nn
+	if (p < 0)
+		p = 0;
+	if (p > 1)
+		p = 1;
+	p *= datasetSize;
+	return p;
+}
+
+vector<double> net::Relu(vector<double> input)
+{
+	for (int i = 0; i < input.size(); i++)
+	{
+		input[i] = max(double(0), input[i]);
+	}
+	return input;
+}
+
+#endif
