a6f89f0f7a27f82e87d5e7ffb4b4d26702d6ce2c
JY <459294193@qq.com>
2020-04-08 23:48:04 +0800

    重构最初版本

--- src/reconstruction.h
+++ src/reconstruction.h
@@ -10,6 +10,7 @@
 #include "./leafNode/normalNode.h"
 #include "./dataset/lognormalDistribution.h"
 #include "./dataset/uniformDistribution.h"
+#include "node.h"
 
 #include <algorithm>
 #include <iostream>
@@ -19,10 +20,15 @@ using namespace std;
 
 void reconstruction(const vector<pair<double, double>> &data, const vector<pair<int, int>> &cnt)
 {
+    cout << endl;
     cout << "-------------------------------" << endl;
     cout << "Start reconstruction!" << endl;
+    node root;
+
     int datasetSize = data.size();
-    vector<pair<double, double>> dataset = data;
+    btree::btree_map<double, pair<int, int>> cntTree;
+    for (int i = 0; i < cnt.size(); i++)
+        cntTree.insert(dataset[i].first, cnt[i]); // <key, <read, write>>
     int totalReadNum = 0, totalWriteNum = 0;
     for (int i = 0; i < datasetSize; i++)
     {
@@ -38,50 +44,51 @@ void reconstruction(const vector<pair<double, double>> &data, const vector<pair<
     params firstStageParams(0.00001, 5000, 8, 0.0001, 0.00001);
     params secondStageParams(0.0000001, 1, 10000, 8, 0.0, 0.0);
 
-    if ((float)totalReadNum / (float)totalWriteNum >= 4.0)
-    {
-        // use static inner node
-        staticRMI<normalNode<linearRegression>, linearRegression> SRMI_normal;
-
-        // SRMI_normal = staticRMI<normalNode<linearRegression>, linearRegression>(dataset, firstStageParams, secondStageParams, 2000, 15, 800);
-        SRMI_normal = staticRMI<normalNode<linearRegression>, linearRegression>(dataset, firstStageParams, secondStageParams, 2000, 15, 800);
-        SRMI_normal.train();
-        cout << "SRMI_normal init over!" << endl;
-        cout << "****************" << endl;
-        SRMI_normal.change(cnt, 2000, secondStageParams, 800);
-
-        //test
-        LARGE_INTEGER s, e, c;
-        QueryPerformanceCounter(&s);
-        QueryPerformanceFrequency(&c);
-        for (int i = 0; i < dataset.size(); i++)
-        {
-            SRMI_normal.find(dataset[i].first);
-        }
-        QueryPerformanceCounter(&e);
-        cout << "Find time:" << (double)(e.QuadPart - s.QuadPart) / (double)c.QuadPart / (float)dataset.size() << endl;
-    }
+    // Select the innerNode model
+    bool rootIsScale = true;
+    if (rootIsScale)
+        root = scaleModel<gappedNode<linearRegression>>(secondStageParams, 1000, 100, 800);
     else
-    {
-        scaleModel<normalNode<linearRegression>> SCALE_normal;
-        // SCALE_normal = scaleModel<normalNode<linearRegression>>(secondStageParams, 1000, 100, 800);
-        SCALE_normal = scaleModel<normalNode<linearRegression>>(secondStageParams, 1000, 100, 800);
-        SCALE_normal.initialize(dataset);
-        cout << "SCALE_normal init over!" << endl;
-        cout << "****************" << endl;
-        SCALE_normal.change(cnt, 1000, secondStageParams, 800);
+        root = adaptiveRMI<gappedNode<linearRegression>, linearRegression>(firstStageParams, secondStageParams, 1000, 12, 800);
+    cout << "root init over!" << endl;
 
-        //test
-        LARGE_INTEGER s, e, c;
-        QueryPerformanceCounter(&s);
-        QueryPerformanceFrequency(&c);
-        for (int i = 0; i < dataset.size(); i++)
+    // Find the minimum childNum
+    long double minCost = 1e100;
+    int minNum = 0;
+    for (int i = 2; i < dataset.size(); i++)
+    {
+        long double tmpCost = root.getCost(cntTree, i);
+        if (tmpCost < minCost)
         {
-            SCALE_normal.find(dataset[i].first);
+            minCost = tmpCost;
+            minNum = i;
         }
-        QueryPerformanceCounter(&e);
-        cout << "Find time:" << (double)(e.QuadPart - s.QuadPart) / (double)c.QuadPart / (float)dataset.size() << endl;
     }
+    cout << "find the minimum childNum: " << minNum << endl;
+
+    // Rebuild the tree according to the calculated most suitable childNum
+    int threshold = float(dataset.size() * 1.2) / minNum;
+    int capacity = threshold * 0.8;
+    if (rootIsScale)
+        root = scaleModel<gappedNode<linearRegression>>(secondStageParams, threshold, minNum, capacity);
+    else
+        root = adaptiveRMI<gappedNode<linearRegression>, linearRegression>(firstStageParams, secondStageParams, threshold, minNum, capacity);
+    cout << "Rebuild root over!" << endl;
+
+    // Adjust the structure of leaf nodes
+    root.change(cntTree);
+    cout << "Adjust leaf nodes over!" << endl;
+
+    //test
+    LARGE_INTEGER s, e, c;
+    QueryPerformanceCounter(&s);
+    QueryPerformanceFrequency(&c);
+    for (int i = 0; i < dataset.size(); i++)
+    {
+        root.find(dataset[i].first);
+    }
+    QueryPerformanceCounter(&e);
+    cout << "Find time:" << (double)(e.QuadPart - s.QuadPart) / (double)c.QuadPart / (float)dataset.size() << endl;
 }
 
 #endif
