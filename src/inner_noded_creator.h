bfb493a382827f6d07d80905f6f5f4adc15606e5
JY <459294193@qq.com>
2020-05-02 01:44:10 +0800

    nodeCreator单独列为头文件

---
+++ src/inner_noded_creator.h
@@ -0,0 +1,26 @@
+#include "./innerNode/lr_node.h"
+#include "./innerNode/nn_node.h"
+#include "./innerNode/binary_search.h"
+#include "./innerNode/division_node.h"
+#include "innerNode/inner_node.h"
+
+BasicInnerNode *InnerNodeCreator(int innerNodeType, int childNum)
+{
+    BasicInnerNode *newNode;
+    switch (innerNodeType)
+    {
+    case 0:
+        newNode = new AdaptiveLR(childNum);
+        break;
+    case 1:
+        newNode = new AdaptiveNN(childNum);
+        break;
+    case 2:
+        newNode = new AdaptiveDiv(childNum);
+        break;
+    case 3:
+        newNode = new AdaptiveBin(childNum);
+        break;
+    }
+    return newNode;
+}
