2a9526ad92aed88b289b40b2a9eaea00c97ab6af
JY <459294193@qq.com>
2020-05-02 01:45:13 +0800

    将参数集中在该文件中

--- src/params.h
+++ src/params.h
@@ -1,11 +1,21 @@
 #ifndef PARAMS
 #define PARAMS
+#include "leafNode/array.h"
+#include "leafNode/gapped_array.h"
 
-// uniform distribution
-const int max_epoch = 500;
-const double learning_rate = 0.0001;
-// const int neuron_number = 8;
-const int neuron_number = 1;
+#define LEAF_NODE_TYPE ArrayNode
+// #define LEAF_NODE_TYPE GappedArray
 
+extern int kLeafNodeID;
+extern int kInnerNodeID;
+
+extern const int kMaxEpoch = 500;
+extern const double kLearningRate = 0.0001;
+// const int kNeuronNumber = 8;
+extern const int kNeuronNumber = 1;
+
+extern const int kThreshold = 400;
+extern const double kDensity = 0.5;
+extern const int kMaxKeyNum = 10000;
 
 #endif
