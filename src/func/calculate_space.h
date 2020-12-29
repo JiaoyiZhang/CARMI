2041bd9688abb2878dc4488aa9878c63faa14505
JY <459294193@qq.com>
2020-12-29 14:09:03 +0800

    更新相关函数

--- src/func/calculate_space.h
+++ src/func/calculate_space.h
@@ -8,14 +8,12 @@
 #include "../innerNodeType/nn_type.h"
 #include "../leafNodeType/ga_type.h"
 #include "../leafNodeType/array_type.h"
-using namespace std;
 
-extern vector<LRType> LRVector;
-extern vector<NNType> NNVector;
-extern vector<HisType> HisVector;
-extern vector<BSType> BSVector;
-extern vector<ArrayType> ArrayVector;
-extern vector<GappedArrayType> GAVector;
+#include "../innerNode/bs_model.h"
+#include "../innerNode/lr_model.h"
+#include "../innerNode/nn_model.h"
+#include "../innerNode/his_model.h"
+using namespace std;
 
 extern vector<pair<double, double>> dataset;
 extern vector<pair<double, double>> insertDataset;
@@ -27,21 +25,24 @@ long double calculateSpace()
     cout << "NNType:" << sizeof(NNType) << "\tnn model:" << sizeof(PiecewiseLR) << endl;
     cout << "HisType:" << sizeof(HisType) << "\this model:" << sizeof(HistogramModel) << endl;
     cout << "BSType:" << sizeof(BSType) << "\tbs model:" << sizeof(BinarySearchModel) << endl;
+    cout << "LRModel:" << sizeof(LRModel) << endl;
+    cout << "NNModel:" << sizeof(NNModel) << endl;
+    cout << "HisModel:" << sizeof(HisModel) << endl;
+    cout << "BSModel:" << sizeof(BSModel) << endl;
     cout << "ArrayType:" << sizeof(ArrayType) << endl;
     cout << "GappedArrayType:" << sizeof(GappedArrayType) << endl;
-    
+
     long double space = 0;
 
-    for (int i = 0; i < LRVector.size(); i++)
-        space += sizeof(LRType) + LRVector[i].childNumber * 4;
-    for (int i = 0; i < NNVector.size(); i++)
-        space += sizeof(NNType) + NNVector[i].childNumber * 4 + 192;
-    for (int i = 0; i < HisVector.size(); i++)
-        space += sizeof(HisType) + HisVector[i].childNumber * 5;
-    for (int i = 0; i < BSVector.size(); i++)
-        space += sizeof(BSType) + BSVector[i].childNumber * 12;
-    space += sizeof(ArrayType) * ArrayVector.size();
-    space += sizeof(GappedArrayType) * GAVector.size();
+    for (int i = 0; i < nowChildNumber; i++)
+    {
+        if (entireChild[i]->flag < 'E')
+            space += 64;
+        else if (entireChild[i]->flag == 'E')
+            space += sizeof(ArrayType);
+        else
+            space += sizeof(GappedArrayType);
+    }
     space = space / 1024 / 1024;
     cout << "\nTOTAL SPACE: " << space << "MB" << endl;
     space += float(dataset.size()) * 16 / 1024 / 1024;
