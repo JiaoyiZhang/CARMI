e1c8e273236617814949c91ba77c5080f9b0c02b
JY <459294193@qq.com>
2021-02-22 17:42:09 +0800

    add NodeCost, RootStruct structs

---
+++ src/CARMI/construct/node_cost_struct.h
@@ -0,0 +1,11 @@
+#ifndef NODE_COST_STRUCT_H
+#define NODE_COST_STRUCT_H
+
+struct NodeCost
+{
+    double time;
+    double space;
+    bool isInnerNode;
+};
+
+#endif // !NODE_COST_STRUCT_H
---
+++ src/CARMI/construct/root_struct.h
@@ -0,0 +1,10 @@
+#ifndef ROOT_STRUCT_H
+#define ROOT_STRUCT_H
+
+struct RootStruct
+{
+    int rootType;
+    int rootChildNum;
+};
+
+#endif // !ROOT_STRUCT_H
