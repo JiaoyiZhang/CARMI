88e0d79b080313476c828f3efd4f4b2ff0357e74
JY <459294193@qq.com>
2021-03-05 21:52:18 +0800

    update construction & store_node

--- src/CARMI/construct/construction.h
+++ src/CARMI/construct/construction.h
@@ -1,141 +1,77 @@
 #ifndef CONSTRUCTION_H
 #define CONSTRUCTION_H
 
-#include "root_struct.h"
-#include "params_struct.h"
-#include "choose_root.h"
+#include "structures.h"
 #include "construct_root.h"
-#include "update_leaf.h"
 #include "../carmi.h"
 #include <vector>
 #include <map>
 using namespace std;
 
-// main function of construction
-// return the type of root
-// findDatapoint: the dataset used to initialize the index
-// insertDatapoint: the dataset to be inserted into the index
-// readCnt: the number of READ corresponding to each key
-// writeCnt: the number of WRITE corresponding to each key
+inline void CARMI::ConstructSubTree(RootStruct *rootStruct, SubDataset *subDataset, NodeCost *nodeCost)
+{
+    for (int i = 0; i < rootStruct->rootChildNum; i++)
+    {
+        NodeCost resChild;
+        DataRange *range = new DataRange(subDataset->subInit.subLeft[i], subDataset->subInit.subSize[i], subDataset->subFind.subLeft[i], subDataset->subFind.subSize[i], subDataset->subInsert.subLeft[i], subDataset->subInsert.subSize[i]);
+        if (subDataset->subInit.subSize[i] + subDataset->subInsert.subSize[i] > kAlgorithmThreshold)
+            resChild = GreedyAlgorithm(range);
+        else
+            resChild = dp(range);
+        int type;
+        MapKey key = {resChild.isInnerNode, {subDataset->subInit.subLeft[i], subDataset->subInit.subSize[i]}};
+        auto it = structMap.find(key);
+        type = it->second.type;
+
+        storeOptimalNode(type, &key, range, i);
+
+        nodeCost->cost += resChild.space + resChild.time;
+        nodeCost->time += resChild.time;
+        nodeCost->space += resChild.space;
+
+        COST.clear();
+        structMap.clear();
+    }
+}
+
+/**
+ * @brief main function of construction
+ * @param initData the dataset used to initialize the index
+ * @param findData the find queries used to training CARMI
+ * @param insertData the insert queries used to training CARMI
+ * @return the type of root
+ */
 inline int CARMI::Construction(const vector<pair<double, double>> &initData, const vector<pair<double, double>> &findData, const vector<pair<double, double>> &insertData)
 {
-#ifdef DEBUG
-    cout << endl;
-    cout << "-------------------------------" << endl;
-    cout << "Start construction!" << endl;
-    querySize = findData.size() + insertData.size();
-    time_t timep;
-    time(&timep);
-    char tmpTime[64];
-    strftime(tmpTime, sizeof(tmpTime), "%Y-%m-%d %H:%M:%S", localtime(&timep));
-    cout << "\nTEST time: " << tmpTime << endl;
-#endif
+    NodeCost nodeCost(0, 0, 0, true);
+    RootStruct *res = ChooseRoot();
+    rootType = res->rootType;
+    SubDataset *subDataset = StoreRoot(res, &nodeCost);
 
-    if (initDataset.size() != initData.size())
+    // COST.insert({{-1, 0}, {0, 0}});
+    // ParamStruct leafP;
+    // leafP.type = 5;
+    // leafP.density = 0.5;
+    // structMap.insert({{false, {-1, 0}}, leafP});
+    ConstructSubTree(res, subDataset, &nodeCost);
+    UpdateLeaf();
+
+    // TODO
+    if (kPrimaryIndex)
     {
-        initDataset = initData;
-        findQuery = findData;
-        insertQuery = insertData;
+        vector<pair<double, double>> tmp(100000, {DBL_MIN, DBL_MIN});
+        initDataset.insert(initDataset.end(), tmp.begin(), tmp.end());
     }
-
-    if (!kPrimaryIndex)
-        initEntireData(0, initDataset.size(), false);
-    initEntireChild(initDataset.size());
-
-    auto res = ChooseRoot();
+    else
+        vector<pair<double, double>>().swap(initDataset);
+    vector<pair<double, double>>().swap(findQuery);
+    vector<pair<double, double>>().swap(insertQuery);
+    entireData.erase(entireData.begin() + nowDataSize + reservedSpace, entireData.end());
 
     COST.clear();
     structMap.clear();
     scanLeaf.clear();
 
-    int childNum = res.rootChildNum;
-    rootType = res.rootType;
-    kInnerNodeID = rootType;
-
-#ifdef DEBUG
-    cout << "Construction of the root node has been completed!" << endl;
-    cout << "The optimal type of root is: " << res.rootType << ",\tthe optimal child number is: " << res.rootChildNum << endl;
-#endif
-
-    double totalCost = 0.0;
-    double totalTime = 0.0;
-    double totalSpace = 0.0;
-
-#ifdef DEBUG
-    time(&timep);
-    char tmpTime2[64];
-    strftime(tmpTime2, sizeof(tmpTime2), "%Y-%m-%d %H:%M:%S", localtime(&timep));
-    cout << "Root time: " << tmpTime2 << endl;
-#endif
-
-    COST.insert({{-1, 0}, {0, 0}});
-    ParamStruct leafP;
-    leafP.type = 5;
-    leafP.density = 0.5;
-    structMap.insert({{false, {-1, 0}}, leafP});
-
-    switch (rootType)
-    {
-    case 0:
-    {
-        root.lrRoot = LRType(childNum);
-        root.lrRoot.childLeft = allocateChildMemory(childNum);
-        root.lrRoot.model.Train(initDataset, childNum);
-        totalTime = 12.7013;
-        totalSpace += sizeof(LRType);
-        ConstructRoot<LRType>(&root.lrRoot, childNum, totalCost, totalTime, totalSpace);
-        break;
-    }
-    case 1:
-    {
-        root.plrRoot = PLRType(childNum);
-        root.plrRoot.childLeft = allocateChildMemory(childNum);
-        root.plrRoot.model.Train(initDataset, childNum);
-        totalTime = 39.6429;
-        totalSpace += sizeof(PLRType);
-        ConstructRoot<PLRType>(&root.plrRoot, childNum, totalCost, totalTime, totalSpace);
-        break;
-    }
-    case 2:
-    {
-        root.hisRoot = HisType(childNum);
-        root.hisRoot.childLeft = allocateChildMemory(childNum);
-        root.hisRoot.model.Train(initDataset, childNum);
-        totalTime = 44.2824;
-        totalSpace += sizeof(HisType);
-        ConstructRoot<HisType>(&root.hisRoot, childNum, totalCost, totalTime, totalSpace);
-        break;
-    }
-    case 3:
-    {
-        root.bsRoot = BSType(childNum);
-        root.bsRoot.childLeft = allocateChildMemory(childNum);
-        root.bsRoot.model.Train(initDataset, childNum);
-        totalTime = 10.9438 * log(childNum) / log(2);
-        totalSpace += sizeof(BSType);
-        ConstructRoot<BSType>(&root.bsRoot, childNum, totalCost, totalTime, totalSpace);
-        break;
-    }
-    }
-    UpdateLeaf();
-
-#ifdef DEBUG
-    cout << "total cost: " << totalCost << endl;
-    cout << "total time: " << totalTime << endl;
-    cout << "total space: " << totalSpace << endl;
-
-    time(&timep);
-    char tmpTime1[64];
-    strftime(tmpTime1, sizeof(tmpTime1), "%Y-%m-%d %H:%M:%S", localtime(&timep));
-    cout << "finish time: " << tmpTime1 << endl;
-#endif
-    vector<pair<double, double>> tmp(100000, {DBL_MIN, DBL_MIN});
-    initDataset.insert(initDataset.end(), tmp.begin(), tmp.end());
-    if (!kPrimaryIndex)
-        vector<pair<double, double>>().swap(initDataset);
-    vector<pair<double, double>>().swap(findQuery);
-    vector<pair<double, double>>().swap(insertQuery);
-    entireData.erase(entireData.begin() + nowDataSize + 1024 * 1024, entireData.end());
     return rootType;
 }
 
--- src/CARMI/construct/store_node.h
+++ src/CARMI/construct/store_node.h
@@ -19,22 +19,16 @@
 #include <set>
 using namespace std;
 template <typename TYPE>
-TYPE CARMI::storeInnerNode(pair<bool, pair<int, int>> key, const int left, const int size, const int insertLeft, const int insertSize, int storeIdx)
+TYPE *CARMI::storeInnerNode(MapKey *key, DataRange *range, int storeIdx)
 {
-    auto it = structMap.find(key);
+    auto it = structMap.find(*key);
     if (it == structMap.end())
         cout << "WRONG!" << endl;
-    TYPE node = TYPE();
-    node.SetChildNumber(it->second.childNum);
-    int optimalChildNumber = it->second.childNum;
-    node.childLeft = allocateChildMemory(optimalChildNumber);
-    Train(&node, left, size);
 
-    // divide the key and query
-    vector<pair<int, int>> subInitData(optimalChildNumber, {-1, 0});
-    vector<pair<int, int>> subFindData(optimalChildNumber, {-1, 0});
-    vector<pair<int, int>> subInsertData(optimalChildNumber, {-1, 0});
-    InnerDivide<TYPE>(&node, optimalChildNumber, left, size, left, size, insertLeft, insertSize, subInitData, subFindData, subInsertData, true);
+    int optimalChildNumber = it->second.childNum;
+    SubDataset *subDataset = new SubDataset(optimalChildNumber);
+    TYPE *node = InnerDivideAll<TYPE>(optimalChildNumber, range, subDataset);
+    node->childLeft = allocateChildMemory(optimalChildNumber);
 
     for (int i = 0; i < optimalChildNumber; i++)
     {
@@ -45,27 +39,29 @@ TYPE CARMI::storeInnerNode(pair<bool, pair<int, int>> key, const int left, const
             type = 4;
         else
             type = iter->second.type;
-        storeOptimalNode(type, nowKey, subInitData[i].first, subInitData[i].second, subInsertData[i].first, subInsertData[i].second, node.childLeft + i);
+        DataRange *subRange = new DataRange(subDataset->subInit.subLeft[i], subDataset->subInit.subSize[i], subDataset->subFind.subLeft[i], subDataset->subFind.subSize[i], subDataset->subInsert.subLeft[i], subDataset->subInsert.subSize[i]);
+        storeOptimalNode(type, &nowKey, subRange, node->childLeft + i);
     }
     return node;
 }
 
 // store the optimal node into the index structure
 // tmpIdx: key in the corresponding struct
-void CARMI::storeOptimalNode(int optimalType, pair<bool, pair<int, int>> key, const int left, const int size, const int insertLeft, const int insertSize, int storeIdx)
+void CARMI::storeOptimalNode(int optimalType, MapKey *key, DataRange *range, int storeIdx)
 {
-    if (size == 0)
+    // cout<<"construct size:"<<size<<",\ttype:"<<optimalType<<endl;
+    if (range->initRange.size == 0)
     {
         if (kPrimaryIndex)
         {
             auto node = YCSBLeaf();
-            initYCSB(&node, left, size);
+            initYCSB(&node, range->initRange.left, range->initRange.size);
             entireChild[storeIdx].ycsbLeaf = node;
         }
         else
         {
-            auto node = GappedArrayType(max(size, kThreshold));
-            initGA(&node, left, size);
+            auto node = GappedArrayType(kThreshold);
+            initGA(&node, range->initRange.left, range->initRange.size);
             entireChild[storeIdx].ga = node;
 
 #ifdef DEBUG
@@ -80,55 +76,55 @@ void CARMI::storeOptimalNode(int optimalType, pair<bool, pair<int, int>> key, co
     {
     case 0:
     {
-        auto node = storeInnerNode<LRModel>(key, left, size, insertLeft, insertSize, storeIdx);
-        entireChild[storeIdx].lr = node;
+        auto node = storeInnerNode<LRModel>(key, range, storeIdx);
+        entireChild[storeIdx].lr = *node;
         break;
     }
     case 1:
     {
-        auto node = storeInnerNode<PLRModel>(key, left, size, insertLeft, insertSize, storeIdx);
-        entireChild[storeIdx].plr = node;
+        auto node = storeInnerNode<PLRModel>(key, range, storeIdx);
+        entireChild[storeIdx].plr = *node;
         break;
     }
     case 2:
     {
-        auto node = storeInnerNode<HisModel>(key, left, size, insertLeft, insertSize, storeIdx);
-        entireChild[storeIdx].his = node;
+        auto node = storeInnerNode<HisModel>(key, range, storeIdx);
+        entireChild[storeIdx].his = *node;
         break;
     }
     case 3:
     {
-        auto node = storeInnerNode<BSModel>(key, left, size, insertLeft, insertSize, storeIdx);
-        entireChild[storeIdx].bs = node;
+        auto node = storeInnerNode<BSModel>(key, range, storeIdx);
+        entireChild[storeIdx].bs = *node;
         break;
     }
     case 4:
     {
-        auto node = ArrayType(max(size, kThreshold));
-        initArray(&node, left, size);
+        auto node = ArrayType(max(range->initRange.size, kThreshold));
+        initArray(&node, range->initRange.left, range->initRange.size);
         entireChild[storeIdx].array = node;
-        if (size > 0)
-            scanLeaf.insert({initDataset[left].first, storeIdx});
+        if (range->initRange.size > 0)
+            scanLeaf.insert({initDataset[range->initRange.left].first, storeIdx});
         break;
     }
     case 5:
     {
-        auto it = structMap.find(key);
+        auto it = structMap.find(*key);
         if (it == structMap.end())
             cout << "WRONG!" << endl;
 
-        auto node = GappedArrayType(max(size, kThreshold));
+        auto node = GappedArrayType(kThreshold);
         node.density = it->second.density;
-        initGA(&node, left, size);
+        initGA(&node, range->initRange.left, range->initRange.size);
         entireChild[storeIdx].ga = node;
-        if (size > 0)
-            scanLeaf.insert({initDataset[left].first, storeIdx});
+        if (range->initRange.size > 0)
+            scanLeaf.insert({initDataset[range->initRange.left].first, storeIdx});
         break;
     }
     case 6:
     {
         auto node = YCSBLeaf();
-        initYCSB(&node, left, size);
+        initYCSB(&node, range->initRange.left, range->initRange.size);
         entireChild[storeIdx].ycsbLeaf = node;
         break;
     }
