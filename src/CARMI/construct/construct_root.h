686a9be5290fdfc56f308632761ca9233bfe0958
JY <459294193@qq.com>
2021-03-05 21:50:41 +0800

    update root related files

--- src/CARMI/construct/construct_root.h
+++ src/CARMI/construct/construct_root.h
@@ -3,79 +3,98 @@
 
 #include "../carmi.h"
 #include "store_node.h"
+#include "structures.h"
 #include "dp.h"
+using namespace std;
 
-template <typename TYPE>
-inline void CARMI::ConstructRoot(TYPE *root, const int childNum, double &totalCost, double &totalTime, double &totalSpace)
+template <typename TYPE, typename ModelType>
+void CARMI::CheckRoot(int c, int type, double &optimalCost, double time, RootStruct *rootStruct)
 {
-    vector<pair<int, int>> subInitData(childNum, {-1, 0});
-    vector<pair<int, int>> subFindData(childNum, {-1, 0});
-    vector<pair<int, int>> subInsertData(childNum, {-1, 0});
-    for (int i = 0; i < initDataset.size(); i++)
-    {
-        int p = root->model.Predict(initDataset[i].first);
-        if (subInitData[p].first == -1)
-            subInitData[p].first = i;
-        subInitData[p].second++;
-    }
-    for (int i = 0; i < findQuery.size(); i++)
-    {
-        int p = root->model.Predict(findQuery[i].first);
-        if (subFindData[p].first == -1)
-            subFindData[p].first = i;
-        subFindData[p].second++;
-    }
-    for (int i = 0; i < insertQuery.size(); i++)
-    {
-        int p = root->model.Predict(insertQuery[i].first);
-        if (subInsertData[p].first == -1)
-            subInsertData[p].first = i;
-        subInsertData[p].second++;
-    }
+    SubSingleDataset perSize(c);
+    double space = 64.0 * c / 1024 / 1024;
 
-    for (int i = 0; i < childNum; i++)
+    TYPE root = TYPE(c);
+    root.model.Train(initDataset, c);
+    SingleDataRange range;
+    range.left = 0;
+    range.size = initDataset.size();
+    NodePartition<ModelType>(&root.model, &perSize, &range, initDataset);
+    double entropy = CalculateEntropy(perSize.subSize, initDataset.size(), c);
+    double cost = (time + float(kRate * space)) / entropy;
+
+    if (cost <= optimalCost)
     {
-        NodeCost resChild;
-        if (subInitData[i].second + subInsertData[i].second > kAlgorithmThreshold)
-            resChild = GreedyAlgorithm(subInitData[i].first, subInitData[i].second, subFindData[i].first, subFindData[i].second, subInsertData[i].first, subInsertData[i].second);
-        else
-            resChild = dp(subInitData[i].first, subInitData[i].second, subFindData[i].first, subFindData[i].second, subInsertData[i].first, subInsertData[i].second);
-        int type;
-        pair<bool, pair<int, int>> key = {resChild.isInnerNode, {subInitData[i].first, subInitData[i].second}};
-        auto it = structMap.find(key);
-        if (it == structMap.end())
-        {
-            if (kPrimaryIndex)
-                type = 6;
-            else
-                type = 4;
-        }
-        else
-            type = it->second.type;
-
-        storeOptimalNode(type, key, subInitData[i].first, subInitData[i].second, subInsertData[i].first, subInsertData[i].second, i);
-
-        totalCost += resChild.space + resChild.time;
-        totalTime += resChild.time;
-        totalSpace += resChild.space;
-
-        COST.clear();
-        structMap.clear();
+        optimalCost = cost;
+        rootStruct = new RootStruct(type, c);
     }
-#ifdef DEBUG
-    cout << "construct over!" << endl;
-    double entropy = 0.0;
-    int size = initDataset.size();
-    float p = 0;
-    for (int i = 0; i < childNum; i++)
-    {
-        p = float(subInitData[i].second) / size;
-        if (p != 0)
-            entropy -= p * log(p) / log(2);
-    }
-    cout << "entropy: " << entropy;
-
-#endif // DEBUG
 }
 
+RootStruct *CARMI::ChooseRoot()
+{
+    double OptimalValue = DBL_MAX;
+    RootStruct *rootStruct;
+    for (int c = 1024; c <= initDataset.size() * 10; c *= 2)
+    {
+        CheckRoot<LRType, LinearRegression>(c, 0, OptimalValue, LRRootTime, rootStruct);
+        CheckRoot<PLRType, PiecewiseLR>(c, 1, OptimalValue, PLRRootTime, rootStruct);
+        CheckRoot<HisType, HistogramModel>(c, 2, OptimalValue, HisRootTime, rootStruct);
+        CheckRoot<BSType, BinarySearchModel>(c, 3, OptimalValue, 10.9438 * log(c) / log(2), rootStruct);
+    }
+#ifdef DEBUG
+    cout << "Best type is: " << rootStruct->rootType << "\tbest childNumber: " << rootStruct->rootChildNum << "\tOptimal Value: " << OptimalValue << endl;
+#endif
+    return rootStruct;
+}
+
+template <typename TYPE>
+TYPE *CARMI::ConstructRoot(RootStruct *rootStruct, SubDataset *subDataset, DataRange *range, int &childLeft)
+{
+    TYPE *root = new TYPE(rootStruct->rootChildNum);
+    childLeft = allocateChildMemory(rootStruct->rootChildNum);
+    root->model.Train(initDataset, rootStruct->rootChildNum);
+
+    NodePartition<TYPE>(root, &(subDataset->subInit), &(range->initRange), initDataset);
+    subDataset->subFind = subDataset->subInit;
+    NodePartition<TYPE>(root, &(subDataset->subInsert), &(range->insertRange), insertQuery);
+    return root;
+}
+
+SubDataset *CARMI::StoreRoot(RootStruct *rootStruct, NodeCost *nodeCost)
+{
+    DataRange dataRange;
+    SubDataset *subDataset = new SubDataset(rootStruct->rootChildNum);
+    int childLeft;
+    switch (rootStruct->rootType)
+    {
+    case 0:
+    {
+        nodeCost->time = LRRootTime;
+        nodeCost->space += sizeof(LRType);
+        root.lrRoot = *ConstructRoot<LRType>(rootStruct, subDataset, &dataRange, childLeft);
+        break;
+    }
+    case 1:
+    {
+        nodeCost->time = PLRRootTime;
+        nodeCost->space += sizeof(PLRType);
+        root.plrRoot = *ConstructRoot<PLRType>(rootStruct, subDataset, &dataRange, childLeft);
+        break;
+    }
+    case 2:
+    {
+        nodeCost->time = HisRootTime;
+        nodeCost->space += sizeof(HisType);
+        root.hisRoot = *ConstructRoot<HisType>(rootStruct, subDataset, &dataRange, childLeft);
+        break;
+    }
+    case 3:
+    {
+        nodeCost->time = 10.9438 * log(rootStruct->rootChildNum) / log(2);
+        nodeCost->space += sizeof(BSType);
+        root.bsRoot = *ConstructRoot<BSType>(rootStruct, subDataset, &dataRange, childLeft);
+        break;
+    }
+    }
+    return subDataset;
+}
 #endif // !CONSTRUCT_ROOT_H
