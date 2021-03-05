556c026448dd994ec3d0ad998082670dffcb799a
JY <459294193@qq.com>
2021-03-05 21:50:52 +0800

    update greedy

--- src/CARMI/construct/greedy.h
+++ src/CARMI/construct/greedy.h
@@ -2,8 +2,7 @@
 #define GREEDY_CHILD_H
 
 #include "../../params.h"
-#include "params_struct.h"
-#include "choose_root.h"
+#include "structures.h"
 #include "dp_inner.h"
 #include "../carmi.h"
 
@@ -14,168 +13,101 @@
 using namespace std;
 
 template <typename TYPE>
-void CARMI::InnerAllocSize(vector<int> &perSize, const int c, const int initLeft, const int initSize)
+void CARMI::CheckGreedy(int c, int type, NodeCost *optimalCost, double time, ParamStruct *optimalStruct, SingleDataRange *range, double pi, int frequency)
 {
-    TYPE root = TYPE();
-    root.SetChildNumber(c);
-    Train(&root, initLeft, initSize);
-    int end = initLeft + initSize;
-    for (int i = initLeft; i < end; i++)
+    SubSingleDataset perSize(c);
+    double space = 64.0 * c / 1024 / 1024;
+
+    TYPE node = TYPE();
+    node.SetChildNumber(c);
+    Train(&node, range->left, range->size);
+
+    NodePartition<TYPE>(&node, &perSize, range, initDataset);
+
+    double entropy = CalculateEntropy(perSize.subSize, range->size, c);
+    double cost = (time + float(kRate * space) / pi) / entropy;
+
+    if (cost <= optimalCost->cost)
     {
-        int p = root.Predict(initDataset[i].first);
-        perSize[p]++;
+        optimalStruct->type = type;
+        optimalStruct->childNum = c;
+        optimalCost = new NodeCost(time * frequency / querySize, kRate * space, cost, true);
     }
 }
-
-NodeCost CARMI::GreedyAlgorithm(const int initLeft, const int initSize, const int findLeft, const int findSize, const int insertLeft, const int insertSize)
+NodeCost CARMI::GreedyAlgorithm(DataRange *range)
 {
     NodeCost nodeCost;
-    if (initSize == 0 && findSize == 0)
+    if (range->initRange.size == 0 && range->findRange.size == 0)
     {
-        nodeCost.space = 0;
-        nodeCost.time = 0;
-        nodeCost.isInnerNode = false;
+        nodeCost = NodeCost(0, 0, 0, false);
         return nodeCost;
     }
 
-    double OptimalValue = DBL_MAX;
-    double OptimalTime = DBL_MAX;
-    double OptimalSpace = DBL_MAX;
-    double time, space;
-    double pi = float(initSize + insertSize) / querySize;
-    ParamStruct optimalStruct = {0, 32, 2, vector<pair<bool, pair<int, int>>>()};
+    NodeCost *optimalCost = new NodeCost(DBL_MAX, DBL_MAX, DBL_MAX, true);
+    double pi = float(range->findRange.size + range->insertRange.size) / querySize;
+    ParamStruct optimalStruct = {0, 32, 2, vector<MapKey>()};
     int frequency = 0;
-    for (int l = findLeft; l < findLeft + findSize; l++)
+    int findEnd = range->findRange.left + range->findRange.size;
+    for (int l = range->findRange.left; l < findEnd; l++)
         frequency += findQuery[l].second;
-    for (int l = insertLeft; l < insertLeft + insertSize; l++)
+    int insertEnd = range->insertRange.left + range->insertRange.size;
+    for (int l = range->insertRange.left; l < insertEnd; l++)
         frequency += insertQuery[l].second;
-    int tmpEnd = initSize / 2;
+    int tmpEnd = range->initRange.size / 2;
+    SingleDataRange singleRange(range->initRange.left, range->initRange.size);
     for (int c = 16; c < tmpEnd; c *= 2)
     {
-        if (512 * c < initSize)
-            continue;
-        for (int type = 0; type < 4; type++)
-        {
-            space = 64.0 * c / 1024 / 1024;
-
-            if (type == 1)
-                continue;
-            vector<int> perSize(c, 0);
-
-            switch (type)
-            {
-            case 0:
-            {
-                time = 92.4801;
-                InnerAllocSize<LRModel>(perSize, c, initLeft, initSize);
-                break;
-            }
-            case 1:
-            {
-                time = 97.1858;
-                InnerAllocSize<PLRModel>(perSize, c, initLeft, initSize);
-                break;
-            }
-            case 2:
-            {
-                if (c > 160)
-                    break;
-                time = 109.8874;
-                InnerAllocSize<HisModel>(perSize, c, initLeft, initSize);
-                break;
-            }
-            case 3:
-            {
-                if (c > 20)
-                    break;
-                time = 114.371;
-                InnerAllocSize<BSModel>(perSize, c, initLeft, initSize);
-                break;
-            }
-            }
-
-            long double entropy = 0.0;
-            for (int i = 0; i < c; i++)
-            {
-                auto p = float(perSize[i]) / initSize;
-                if (p != 0)
-                    entropy += p * (-log(p) / log(2));
-            }
-            // entropy /= (log(c) / log(2));
-
-            double cost = (time + float(kRate * space) / pi) / entropy;
-            if (cost <= OptimalValue)
-            {
-                OptimalValue = cost;
-                OptimalSpace = float(kRate * space);
-                OptimalTime = time * frequency / querySize;
-                optimalStruct.type = type;
-                optimalStruct.childNum = c;
-            }
-        }
+        CheckGreedy<LRModel>(c, 0, optimalCost, LRInnerTime, &optimalStruct, &(range->initRange), pi, frequency);
+        CheckGreedy<PLRModel>(c, 1, optimalCost, PLRInnerTime, &optimalStruct, &(range->initRange), pi, frequency);
+        CheckGreedy<HisModel>(c, 2, optimalCost, HisInnerTime, &optimalStruct, &(range->initRange), pi, frequency);
+        CheckGreedy<BSModel>(c, 3, optimalCost, BSInnerTime, &optimalStruct, &(range->initRange), pi, frequency);
     }
 
     // construct child
-    vector<pair<bool, pair<int, int>>> tmpChild;
-    int childNum = optimalStruct.childNum;
-    vector<pair<int, int>> subInitData(childNum, {-1, 0});   // {left, size}
-    vector<pair<int, int>> subFindData(childNum, {-1, 0});   // {left, size}
-    vector<pair<int, int>> subInsertData(childNum, {-1, 0}); // {left, size}
+    SubDataset *subDataset = new SubDataset(optimalStruct.childNum);
     switch (optimalStruct.type)
     {
     case 0:
     {
-        auto node = LRModel();
-        node.SetChildNumber(childNum);
-        Train(&node, initLeft, initSize);
-        InnerDivide<LRModel>(&node, childNum, initLeft, initSize, findLeft, findSize, insertLeft, insertSize, subInitData, subFindData, subInsertData, true);
+        InnerDivideAll<LRModel>(optimalStruct.childNum, range, subDataset);
         break;
     }
     case 1:
     {
-        auto node = PLRModel();
-        node.SetChildNumber(childNum);
-        Train(&node, initLeft, initSize);
-        InnerDivide<PLRModel>(&node, childNum, initLeft, initSize, findLeft, findSize, insertLeft, insertSize, subInitData, subFindData, subInsertData, true);
+        InnerDivideAll<PLRModel>(optimalStruct.childNum, range, subDataset);
         break;
     }
     case 2:
     {
-        auto node = HisModel();
-        node.SetChildNumber(childNum);
-        Train(&node, initLeft, initSize);
-        InnerDivide<HisModel>(&node, childNum, initLeft, initSize, findLeft, findSize, insertLeft, insertSize, subInitData, subFindData, subInsertData, true);
+        InnerDivideAll<HisModel>(optimalStruct.childNum, range, subDataset);
         break;
     }
     case 3:
     {
-        auto node = BSModel();
-        node.SetChildNumber(childNum);
-        Train(&node, initLeft, initSize);
-        InnerDivide<BSModel>(&node, childNum, initLeft, initSize, findLeft, findSize, insertLeft, insertSize, subInitData, subFindData, subInsertData, true);
+        InnerDivideAll<BSModel>(optimalStruct.childNum, range, subDataset);
         break;
     }
     }
 
-    for (int i = 0; i < childNum; i++)
+    vector<MapKey> tmpChild;
+    for (int i = 0; i < optimalStruct.childNum; i++)
     {
         NodeCost res;
-        if (subInitData[i].second + subInsertData[i].second > kAlgorithmThreshold)
-            res = GreedyAlgorithm(subInitData[i].first, subInitData[i].second, subFindData[i].first, subFindData[i].second, subInsertData[i].first, subInsertData[i].second); // construct an inner node
+        DataRange *range = new DataRange(subDataset->subInit.subLeft[i], subDataset->subInit.subSize[i], subDataset->subFind.subLeft[i], subDataset->subFind.subSize[i], subDataset->subInsert.subLeft[i], subDataset->subInsert.subSize[i]);
+        if (subDataset->subInit.subSize[i] + subDataset->subInsert.subSize[i] > kAlgorithmThreshold)
+            res = GreedyAlgorithm(range);
         else
-            res = dp(subInitData[i].first, subInitData[i].second, subFindData[i].first, subFindData[i].second, subInsertData[i].first, subInsertData[i].second);
-        tmpChild.push_back({res.isInnerNode, {subFindData[i].first, subFindData[i].second}});
-        OptimalValue += res.space + res.time;
-        OptimalSpace += res.space;
-        OptimalTime += res.time;
+            res = dp(range);
+
+        MapKey key = {res.isInnerNode, {subDataset->subInit.subLeft[i], subDataset->subInit.subSize[i]}};
+        tmpChild.push_back(key);
     }
 
+    MapKey nodeKey = {true, {range->initRange.left, range->initRange.size}};
     optimalStruct.child = tmpChild;
-    if (OptimalTime < DBL_MAX)
-        structMap.insert({{true, {initLeft, initSize}}, optimalStruct});
-    nodeCost.time = OptimalTime;
-    nodeCost.space = OptimalSpace;
-    nodeCost.isInnerNode = true;
+    if (optimalCost->time < DBL_MAX)
+        structMap.insert({nodeKey, optimalStruct});
+    nodeCost = NodeCost(optimalCost->time, optimalCost->space, optimalCost->cost, true);
     return nodeCost;
 }
 #endif // !GREEDY_H
