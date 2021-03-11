f82b2ec0d6b7d3ae728a4350c62ba4ff0026ba04
JY <459294193@qq.com>
2021-03-11 12:09:09 +0800

    move the params from CARMI to params

--- src/params.h
+++ src/params.h
@@ -11,4 +11,21 @@ bool kPrimaryIndex = false;
 const double kDensity = 0.5;        // the density of gap in gapped array
 const double kExpansionScale = 1.5; // scale of array expansion
 
+const double BaseNodeSpace = 64.0 / 1024 / 1024;
+
+const double LRRootTime = 12.7013;
+const double PLRRootTime = 39.6429;
+const double HisRootTime = 44.2824;
+
+const double LRInnerTime = 92.4801;
+const double PLRInnerTime = 97.1858;
+const double HisInnerTime = 109.8874;
+const double BSInnerTime = 114.371;
+
+const double CostMoveTime = 6.25;
+const double CostBaseTime = 161.241;
+const double CostBSTime = 10.9438;
+
+const int reservedSpace = 1024 * 512;
+
 #endif
