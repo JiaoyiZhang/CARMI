f47ccf64bc119a290cdbf2bb9fff2272a5d62074
JiaoYiZhang <459294193@qq.com>
2021-10-27 16:30:22 +0800

    add comments to carmi_common

--- src/include/carmi_common.h
+++ src/include/carmi_common.h
@@ -24,16 +24,25 @@
 #include "memoryLayout/node_array.h"
 
 /**
- * @brief The specific common version of the CARMI class template.
+ * @brief The specific common version of the CARMI class template called
+ * CARMICommon. This type of CARMI actually stores and manages data points in
+ * the index structure.
  *
  * The implementation of common CARMI. This class provides users with basic
  * operations such as find, insert, update, and delete, as well as some basic
  * operations for the iterator of the CARMI index. Through the functions of
- * CARMICommon objects, users can construct indexes with good performance for a
- * given dataset without the need of too much space. In addition, during
- * initialization, users can set different lambda values to control the
- * different proportions of time and space costs, so as to achieve a balance
- * between the time and space cost under the corresponding parameters.
+ * CARMICommon objects, users can automatically obtain indexes with good
+ * performance for a given dataset without the need of manual tuning. In
+ * addition, during initialization, users can set different lambda values to
+ * control the different proportions of time and space costs, so as to achieve a
+ * balance between the time and space cost under the corresponding parameters.
+ *
+ * The hybrid construction algorithm in CARMI can automatically construct the
+ * optimal index structures under various datasets with the help of the given
+ * historical find/insert queries. If there are no historical queries, users
+ * only need to pass in the empty objects of the approriate type and CARMI will
+ * default to a read-only workload to construct the optimal index structure
+ * using the initDataset.
  *
  * @tparam KeyType the type of the key
  * @tparam ValueType the type of the value
@@ -44,27 +53,24 @@ class CARMICommon {
   // *** Constructed Types
 
   /**
-   * @brief the type of implementation of CARMI
-   *
+   * @brief the type of the implementation of CARMI
    */
   typedef CARMI<KeyType, ValueType> carmi_impl;
 
   /**
-   * @brief the type of the data point
-   *
+   * @brief the type of the data point: {key, value}
    */
   typedef typename CARMI<KeyType, ValueType>::DataType DataType;
 
   /**
-   * @brief the type of the dataset
-   *
+   * @brief the type of the dataset: [{key_0, value_0}, {key_1, value_1}, ...,
+   * {key_n, value_n}]. The dataset has been sorted.
    */
   typedef typename CARMI<KeyType, ValueType>::DataVectorType DataVectorType;
 
  private:
   /**
    * @brief The contained implementation object of CARMI
-   *
    */
   carmi_impl carmi_tree;
 
@@ -76,55 +82,89 @@ class CARMICommon {
    * [initFirst, initLast) of initDataset and the range [insertFirst,
    * insertLast] of insertDataset.
    *
+   * (1) The constructor uses the two input iterator of initFirst and initLast
+   * to construct the init dataset to train the model in each node and construct
+   * the optimal index structure on it. These data points will be actually
+   * stored in the part of the index tree responsible for managing data points.
+   * In addition, these data points will also be regarded as being accessed once
+   * in historical find queries by default, providing a basis for the
+   * calculation of the cost model.
+   *
+   * (2) Similarly, CARMI uses the two input iterator of insertFirst and
+   * insertLast to construct the insert dataset. These data points will be
+   * regarded as being inserted once in historical insert queries, and are used
+   * to the calculation of the cost model.
+   *
+   * (3) The CARMI index structure can achieve a good performance under the
+   * time/space setting based on the lambda parameter. The goal of the hybrid
+   * construction algorithm is to minimize the total cost of historical access
+   * and insert queries, which is: the average time cost of each query + lambda
+   * times the space cost of the index tree.
+   *
    * @tparam InputIterator the iterator type of input
-   * @param initFirst[in] the first input iterator of initDataset
-   * @param initLast[in] the last input iterator of initDataset
-   * @param insertFirst[in] the first input iterator of insertDataset
-   * @param insertLast[in] the last input iterator of insertDataset
-   * @param lambda[in] lambda (time + lambda * space), used to tradeoff between
-   * time and space cost
+   * @param[in] initFirst the first input iterator of initDataset, used to
+   * construct the init dataset, which will be indexed. The initDataset is also
+   * help for the calculation of the cost model.
+   * @param[in] initLast the last input iterator of initoDataset, used to
+   * construct the init dataset, which will be indexed.
+   * @param[in] insertFirst the first input iterator of insertDataset, used to
+   * construct the insert dataset. The insert dataset is used to calculate the
+   * average insert time cost in the historical insert queries when calculating
+   * the total cost model.
+   * @param[in] insertLast the last input iterator of insertDataset, used to
+   * construct the insert dataset.
+   * @param[in] lambda lambda: cost = (time + lambda * space), used to tradeoff
+   * between time and space cost in the cost model.
    */
   template <typename InputIterator>
   CARMICommon(const InputIterator &initFirst, const InputIterator &initLast,
               const InputIterator &insertFirst, const InputIterator &insertLast,
               double lambda) {
+    // use the given two iterators to get the init dataset in the form of vector
     DataVectorType initDataset;
     PreprocessInput<InputIterator>(initFirst, initLast, &initDataset);
+    // use the given two iterators to get the init dataset in the form of vector
     DataVectorType insertDataset;
     PreprocessInput<InputIterator>(insertFirst, insertLast, &insertDataset);
+    // construct the findQuery/insertQuery using the two datasets and call the
+    // construction function of carmi_tree to obtain the optimal index structure
     Init(initDataset, insertDataset, lambda);
   }
 
   /**
    * @brief the iterator of CARMICommon items. The iterator points to a specific
-   * position of a data point in the carmi_tree.
+   * position of a data point in the carmi_tree and provides some basic
+   * functions, such as: get the key value, get the data, get the next data
+   * point and so on. Users can use this iterator to get the key/value of a data
+   * point stored in the carmi_tree.
    */
   class iterator {
    public:
     /**
-     * @brief Construct an empty new iterator object
+     * @brief Construct an empty new iterator object with the default values
      */
     inline iterator() : tree(NULL), currnode(NULL), currblock(0), currslot(0) {}
 
     /**
-     * @brief Construct a new iterator object
+     * @brief Construct a new iterator object and set the pointer to the current
+     * carmi index
      *
-     * @param t[in] the pointer of the carmi tree
+     * @param[in] t the pointer of the carmi tree
      */
     explicit inline iterator(CARMICommon *t)
-        : tree(t), currnode(NULL), currblock(0), currslot(-1) {}
+        : tree(t), currnode(NULL), currblock(0), currslot(0) {}
 
     /**
      * @brief Construct a new iterator object
      *
-     * @param t[in] the pointer of the carmi tree
-     * @param n[in] the pointer of the current leaf node
-     * @param u[in] the index of the current data block in the leaf node
-     * @param s[in] the index of the data points in the data block
+     * @param[in] t the pointer of the carmi tree
+     * @param[in] node the pointer of the current leaf node
+     * @param[in] block the index of the current data block in the leaf node
+     * @param[in] slot the index of the data points in the data block
      */
-    inline iterator(CARMICommon *t, BaseNode<KeyType, ValueType> *n, int b,
-                    int s)
-        : tree(t), currnode(n), currblock(b), currslot(s) {}
+    inline iterator(CARMICommon *t, BaseNode<KeyType, ValueType> *node,
+                    int block, int slot)
+        : tree(t), currnode(node), currblock(block), currslot(slot) {}
 
     /**
      * @brief get the key value of this iterator
@@ -134,9 +174,9 @@ class CARMICommon {
      */
     inline const KeyType key() const {
       // Case 1: the current iterator is invalid, return DBL_MAX directly
-      if (currnode == NULL || tree == NULL ||
-          currslot >= CFArrayType<KeyType, ValueType>::kMaxBlockCapacity ||
-          currblock >= CFArrayType<KeyType, ValueType>::kMaxBlockNum) {
+      if (currnode == NULL || tree == NULL || currblock < 0 || currslot < 0 ||
+          currblock >= (currnode->cfArray.flagNumber & 0x00FFFFFF) ||
+          currslot >= CFArrayType<KeyType, ValueType>::kMaxBlockCapacity) {
         return DBL_MAX;
       }
       // Case 2: the current iterator is valid, get the index of the data block
@@ -155,9 +195,9 @@ class CARMICommon {
      */
     inline const ValueType data() const {
       // Case 1: the current iterator is invalid, return DBL_MAX directly
-      if (currnode == NULL || tree == NULL ||
-          currslot >= CFArrayType<KeyType, ValueType>::kMaxBlockCapacity ||
-          currblock >= CFArrayType<KeyType, ValueType>::kMaxBlockNum) {
+      if (currnode == NULL || tree == NULL || currblock < 0 || currslot < 0 ||
+          currblock >= (currnode->cfArray.flagNumber & 0x00FFFFFF) ||
+          currslot >= CFArrayType<KeyType, ValueType>::kMaxBlockCapacity) {
         return DBL_MAX;
       }
       // Case 2: the current iterator is valid, get the index of the data block
@@ -171,7 +211,7 @@ class CARMICommon {
     /**
      * @brief check if the given iterator x is equal to this iterator
      *
-     * @param x[in] the given iterator
+     * @param[in] x the given iterator
      * @return true the given iterator is equal to this iterator
      * @return false the given iterator is unequal to this iterator
      */
@@ -183,7 +223,7 @@ class CARMICommon {
     /**
      * @brief check if the given iterator x is unequal to this iterator
      *
-     * @param x[in] the given iterator
+     * @param[in] x the given iterator
      * @return true the given iterator is unequal to this iterator
      * @return false the given iterator is equal to this iterator
      */
@@ -239,13 +279,11 @@ class CARMICommon {
 
     /**
      * @brief the pointer of the carmi tree
-     *
      */
     CARMICommon *tree;
 
     /**
      * @brief the pointer of the current leaf node
-     *
      */
     BaseNode<KeyType, ValueType> *currnode;
 
@@ -265,12 +303,13 @@ class CARMICommon {
 
   /**
    * @brief preprocess the input dataset between the first iterator and the last
-   * iterator
+   * iterator and construct the dataset in the form of vector
    *
    * @tparam InputIterator the iterator type of input
-   * @param first[in] the first input iterator
-   * @param last[in] the last input iterator
-   * @param initDataset[out] the dataset
+   * @param[in] first the first input iterator
+   * @param[in] last the last input iterator
+   * @param[out] initDataset the constructed dataset: [{key_0, value_0}, {key_1,
+   * value_1}, ..., {key_n, value_n}].
    */
   template <typename InputIterator>
   void PreprocessInput(const InputIterator &first, const InputIterator &last,
@@ -283,11 +322,15 @@ class CARMICommon {
   }
 
   /**
-   * @brief initialize the carmi_tree
+   * @brief construct the findQuery and insertQuery for the cost model and call
+   * the hybrid construction algorithm to initialize the carmi_tree and
+   * construct the optimal CARMI index structure
    *
-   * @param initDataset[in] the init dataset
-   * @param insertDataset[in] the insert dataset
-   * @param lambda[in] lambda (time + lambda * space)
+   * @param[in] initDataset the init dataset: [{key_0, value_0}, {key_1,
+   * value_1}, ..., {key_n, value_n}].
+   * @param[in] insertDataset the insert dataset: [{key_0, value_0}, {key_1,
+   * value_1}, ..., {key_n, value_n}].
+   * @param[in] lambda lambda (time + lambda * space)
    */
   void Init(const DataVectorType &initDataset,
             const DataVectorType &insertDataset, double lambda) {
@@ -295,10 +338,12 @@ class CARMICommon {
     DataVectorType insertQuery = insertDataset;
     std::vector<int> insertQueryIndex;
 
-    // set the frequency
+    // set the frequency, each data point is accessed once in the historical
+    // find queries
     for (int i = 0; i < static_cast<int>(findQuery.size()); i++) {
       findQuery[i].second = 1;
     }
+    // each data point is inserted once in the historical insert queries
     for (int i = 0; i < static_cast<int>(insertDataset.size()); i++) {
       insertQuery[i].second = 1;
     }
@@ -314,7 +359,7 @@ class CARMICommon {
   /**
    * @brief find the corresponding iterator of the given key value
    *
-   * @param key[in] the given key value
+   * @param[in] key the given key value
    * @return iterator the iterator of the data point
    */
   iterator Find(const KeyType &key) {
@@ -326,7 +371,7 @@ class CARMICommon {
   /**
    * @brief insert the given data point into carmi
    *
-   * @param datapoint[in] the inserted data point
+   * @param[in] datapoint the inserted data point
    * @retval true if the operation succeeds
    * @retval false if the operation fails
    */
@@ -337,7 +382,7 @@ class CARMICommon {
   /**
    * @brief update the value of the given data
    *
-   * @param datapoint[in] the new data point
+   * @param[in] datapoint the new data point
    * @retval true if the operation succeeds
    * @retval false if the operation fails
    */
@@ -348,7 +393,7 @@ class CARMICommon {
   /**
    * @brief delete the record of the given key
    *
-   * @param key[in] the key value of the deleted record
+   * @param[in] key the key value of the deleted record
    * @retval true if the operation succeeds
    * @retval false if the operation fails
    */
@@ -360,10 +405,8 @@ class CARMICommon {
    * @return iterator
    */
   iterator begin() {
-    iterator it;
-    it.tree = this;
+    iterator it(this);
     it.currnode = carmi_tree.node.nodeArray[carmi_tree.firstLeaf];
-    it.currslot = 0;
     return it;
   }
 
@@ -372,13 +415,7 @@ class CARMICommon {
    *
    * @return iterator
    */
-  iterator end() {
-    iterator it;
-    it.tree = this;
-    it.currnode = NULL;
-    it.currslot = -1;
-    return it;
-  }
+  iterator end() { return iterator(this); }
 
   /**
    * @brief calculate the space of carmi
